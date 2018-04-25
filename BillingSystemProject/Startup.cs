c1907954d94d8736e11004421b3e59141d5c5006
Umaprasad Bisen <bisenumaprasad@gmail.com>
2018-04-25 19:27:54 +0530

    Initial commit

--- BillingSystemProject/Startup.cs
+++ BillingSystemProject/Startup.cs
@@ -43,7 +43,7 @@ namespace BillingSystemProject
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Home}/{action=Index}");
             });
         }
     }
